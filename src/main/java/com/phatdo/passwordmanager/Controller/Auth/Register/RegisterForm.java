eaeb6add677f21e747ec3358981b8d98458fd846
phatdtgcs220340@fpt.edu.vn <phatdtgcs220340@fpt.edu.vn>
2024-03-24 00:42:47 +0700

    add new form field

--- src/main/java/com/phatdo/passwordmanager/Controller/Auth/Register/RegisterForm.java
+++ src/main/java/com/phatdo/passwordmanager/Controller/Auth/Register/RegisterForm.java
@@ -1,14 +1,15 @@
 package com.phatdo.passwordmanager.Controller.Auth.Register;
 
-import com.phatdo.passwordmanager.User.User;
+import com.phatdo.passwordmanager.Entity.User.User;
 import lombok.Data;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Data
 public class RegisterForm {
+    private String fullName;
     private String username;
     private String password;
     public User toUser(PasswordEncoder passwordEncoder) {
-        return new User(username, passwordEncoder.encode(password));
+        return new User(fullName, username, passwordEncoder.encode(password));
     }
 }
