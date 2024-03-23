318428c9033e6ae90a0a651464948ba3c2065903
phatdtgcs220340@fpt.edu.vn <phatdtgcs220340@fpt.edu.vn>
2024-03-24 00:43:59 +0700

    application repo

---
+++ src/main/java/com/phatdo/passwordmanager/Entity/Application/ApplicationRepository.java
@@ -0,0 +1,10 @@
+package com.phatdo.passwordmanager.Entity.Application;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ApplicationRepository extends JpaRepository<Application, Integer> {
+}
