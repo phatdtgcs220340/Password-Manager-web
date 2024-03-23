ff461538b0bb07241de8701d340db67188873087
phatdtgcs220340@fpt.edu.vn <phatdtgcs220340@fpt.edu.vn>
2024-03-24 00:41:59 +0700

    add logout view

--- src/main/java/com/phatdo/passwordmanager/Config/WebConfig.java
+++ src/main/java/com/phatdo/passwordmanager/Config/WebConfig.java
@@ -16,5 +16,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("/auth/login");
+        registry.addViewController("/logout");
     }
 }
