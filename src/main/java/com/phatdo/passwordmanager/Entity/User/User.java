446ca7e646ec28f1b002145a42ee752ef8e0eb25
phatdtgcs220340@fpt.edu.vn <phatdtgcs220340@fpt.edu.vn>
2024-03-24 00:44:14 +0700

    user entity

---
+++ src/main/java/com/phatdo/passwordmanager/Entity/User/User.java
@@ -0,0 +1,81 @@
+package com.phatdo.passwordmanager.Entity.User;
+
+import java.io.Serializable;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import com.phatdo.passwordmanager.Entity.Application.Application;
+import com.phatdo.passwordmanager.Entity.UserApplication.UserApplication;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+@Data
+@Table(name = "\"user\"")
+@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PUBLIC, force = true)
+public class User implements UserDetails, Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "full_name")
+    private final String fullName;
+    @Column(name = "username", unique = true)
+    private final String username;
+    @Column(name = "password")
+    private final String password;
+    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<UserApplication> userApplications = new HashSet<>();
+    public Set<Application> getApplications() {
+        return this.userApplications.stream()
+                .map(UserApplication::getApplication)
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, fullName, username, password);
+    }
+}
