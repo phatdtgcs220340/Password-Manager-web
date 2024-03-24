c1ed1ffc2b785f791678e01762197f187134f5c7
phatdtgcs220340@fpt.edu.vn <phatdtgcs220340@fpt.edu.vn>
2024-03-24 12:26:39 +0700

    add application service

---
+++ src/main/java/com/phatdo/passwordmanager/Entity/Application/ApplicationService.java
@@ -0,0 +1,14 @@
+package com.phatdo.passwordmanager.Entity.Application;
+
+import com.phatdo.passwordmanager.Entity.User.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class ApplicationService {
+    public Set<Application> listApplication(User user) {
+        return user.getApplications();
+    }
+}
