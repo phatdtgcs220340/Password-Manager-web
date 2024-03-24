74e824213adbdf69c33e3a5d01ad197788b045f0
phatdtgcs220340@fpt.edu.vn <phatdtgcs220340@fpt.edu.vn>
2024-03-24 12:26:05 +0700

    reduce unnecessary method

--- src/main/java/com/phatdo/passwordmanager/Entity/User/UserService.java
+++ src/main/java/com/phatdo/passwordmanager/Entity/User/UserService.java
@@ -1,14 +1,11 @@
 package com.phatdo.passwordmanager.Entity.User;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
 import com.phatdo.passwordmanager.Entity.Application.Application;
 import com.phatdo.passwordmanager.Entity.Application.ApplicationRepository;
-import com.phatdo.passwordmanager.Entity.UserApplication.UserApplication;
-import jakarta.transaction.Transactional;
-import jdk.jshell.spi.ExecutionControl;
+import com.phatdo.passwordmanager.Entity.Account.Account;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -33,18 +30,6 @@ public class UserService implements UserDetailsService {
             throw new Exception();
     }
 
-    public void addApplication(User user, Application application) {
-        // add new application to application list
-        user.getApplications().add(application);
-        UserApplication userApplication = new UserApplication(user, application);
-        // update new assoc instance to assoc table
-        user.getUserApplications().add(userApplication);
-        application.getUserApplications().add(userApplication);
-        applicationRepository.save(application);
-    }
-    public Set<Application> listApplication(User user) {
-        return user.getApplications();
-    }
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<User> optUser = repo.findByUsername(username);
