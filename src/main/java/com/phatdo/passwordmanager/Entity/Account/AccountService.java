9578b09dcee4f9360f7164a0a16472a238b09091
phatdtgcs220340@fpt.edu.vn <phatdtgcs220340@fpt.edu.vn>
2024-03-25 14:18:05 +0700

    add delete account

--- src/main/java/com/phatdo/passwordmanager/Entity/Account/AccountService.java
+++ src/main/java/com/phatdo/passwordmanager/Entity/Account/AccountService.java
@@ -3,7 +3,13 @@ package com.phatdo.passwordmanager.Entity.Account;
 import com.phatdo.passwordmanager.Entity.Application.Application;
 import com.phatdo.passwordmanager.Entity.Application.ApplicationRepository;
 import com.phatdo.passwordmanager.Entity.User.User;
+import com.phatdo.passwordmanager.Entity.User.UserRepository;
+import com.phatdo.passwordmanager.Exception.AccountExistedException;
+
+import jakarta.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,13 +17,38 @@ import java.util.Optional;
 @Service
 public class AccountService {
     private ApplicationRepository applicationRepository;
-    public AccountService(ApplicationRepository applicationRepository) {
+    private AccountRepository accountRepository;
+    private UserRepository userRepository;
+
+    @Autowired
+    public AccountService(ApplicationRepository applicationRepository,
+            AccountRepository accountRepository,
+            UserRepository userRepository) {
         this.applicationRepository = applicationRepository;
+        this.accountRepository = accountRepository;
+        this.userRepository = userRepository;
     }
-    public void addApplication(Account account) {
+
+    public Account findAccount(Integer userId, Integer applicationId) throws EmptyResultDataAccessException {
+        Optional<Account> optAccount = accountRepository.findByUserIdAndApplicationId(userId, applicationId);
+        return optAccount.map(account -> account)
+                .orElseThrow(() -> new EmptyResultDataAccessException("Account not found", 1));
+    }
+
+    public void addApplication(Account account) throws AccountExistedException {
         User user = account.getUser();
         Application application = account.getApplication();
-        Optional<Application> optApplication = applicationRepository.findByApplicationName(application.getApplicationName());
+        // Check if the account is created
+
+        Optional<Application> optApplication = applicationRepository
+                .findByApplicationName(application.getApplicationName());
+        if (optApplication.isPresent()) {
+            if (accountRepository.findByUserIdAndApplicationId(user.getId(), optApplication.get().getId())
+                    .isPresent()) {
+                System.out.println("con cac");
+                throw new AccountExistedException();
+            }
+        }
         optApplication.ifPresentOrElse(existedApplication -> {
             account.setApplication(existedApplication);
             // update new assoc instance to assoc table
@@ -30,8 +61,18 @@ public class AccountService {
             application.getAccounts().add(account);
             applicationRepository.save(application);
         });
-        // add new application to application list
-        user.getApplications().add(account.getApplication());
+        if (!user.getApplications().contains(application))
+            user.getApplications().add(account.getApplication());
         user.getAccounts().add(account);
+    }
+
+    @Transactional
+    public void deleteAssociation(User user, Integer accountId) {
+        Account account = accountRepository.findById(accountId).orElse(null);
+        Application application = account.getApplication();
+        user.getApplications().remove(application);
+        application.getAccounts().remove(account);
+        user.getAccounts().remove(account);
+        userRepository.save(user);
     }
 }
