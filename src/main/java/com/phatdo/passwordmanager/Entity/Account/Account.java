ccbfa5e1807122078d1c47377c39faaa1a0ea84f
phatdtgcs220340@fpt.edu.vn <phatdtgcs220340@fpt.edu.vn>
2024-03-25 14:17:12 +0700

    remove unused stuffs

--- src/main/java/com/phatdo/passwordmanager/Entity/Account/Account.java
+++ src/main/java/com/phatdo/passwordmanager/Entity/Account/Account.java
@@ -14,6 +14,7 @@ import java.util.Objects;
 
 @Entity
 @Data
+@Table(name = "\"account\"")
 @RequiredArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PUBLIC, force = true)
 public class Account implements Serializable {
@@ -38,9 +39,4 @@ public class Account implements Serializable {
     @ManyToOne
     @JoinColumn(name = "application_id")
     private Application application;
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, user, application);
-    }
 }
