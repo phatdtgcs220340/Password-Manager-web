cd15ea25b59494af7b2e6393a612c2142bf8b29b
phatdtgcs220340@fpt.edu.vn <phatdtgcs220340@fpt.edu.vn>
2024-03-25 14:17:44 +0700

    add findByUserIdAndApplicationId

--- src/main/java/com/phatdo/passwordmanager/Entity/Account/AccountRepository.java
+++ src/main/java/com/phatdo/passwordmanager/Entity/Account/AccountRepository.java
@@ -3,6 +3,9 @@ package com.phatdo.passwordmanager.Entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Integer> {
+    Optional<Account> findByUserIdAndApplicationId(Integer userId, Integer applicationId);
 }
