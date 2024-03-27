42c672d087281c26beb749bf5a770a0177758cc8
phatdtgcs220340@fpt.edu.vn <phatdtgcs220340@fpt.edu.vn>
2024-03-27 12:36:03 +0700

    update .yml

--- src/main/java/com/phatdo/passwordmanager/Config/SecurityConfig.java
+++ src/main/java/com/phatdo/passwordmanager/Config/SecurityConfig.java
@@ -1,51 +1,50 @@
 package com.phatdo.passwordmanager.Config;
 
-import com.phatdo.passwordmanager.Entity.User.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Configuration
+@EnableMethodSecurity
 @EnableWebSecurity
 public class SecurityConfig {
-    @Autowired
-    private UserService userService;
+        @Autowired
+        private OAuth2SuccessHandler oAuth2SuccessHandler;
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/home", "/home/**").hasAuthority("ROLE_USER")
-                        .requestMatchers("/","/**").permitAll()
-                        .anyRequest().authenticated())
-                .formLogin(form -> form
-                        .loginPage("/login")
-                        .loginProcessingUrl("/login/authenticate")
-                        .usernameParameter("username")
-                        .passwordParameter("password")
-                        .defaultSuccessUrl("/home",true)
-                        .permitAll())
-                .exceptionHandling(e -> e
-                        .authenticationEntryPoint(
-                                new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
-                .logout(logout -> logout
-                        .logoutUrl("/logout")
-                        .logoutSuccessUrl("/")
-                        .invalidateHttpSession(true)
-                        .deleteCookies("JSESSIONID")
-                        .permitAll());
-        return http.build();
-    }
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+                http
+                                .authorizeHttpRequests((auth) -> auth
+                                                .requestMatchers("/home", "/home/**").hasAuthority("ROLE_USER")
+                                                .requestMatchers("/", "/**").permitAll()
+                                                .anyRequest().authenticated())
+                                .oauth2Login(oauth2 -> oauth2
+                                                .loginPage("/login")
+                                                .loginProcessingUrl("/oauth/authorization/{registerationId}")
+                                                .redirectionEndpoint(endpoint -> endpoint
+                                                                .baseUri("/login/oauth2/code/{registerationId}"))
+                                                .successHandler(oAuth2SuccessHandler))
+                                .exceptionHandling(e -> e
+                                                .authenticationEntryPoint(
+                                                                new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+                                .logout(logout -> logout
+                                                .logoutUrl("/logout")
+                                                .logoutSuccessUrl("/")
+                                                .invalidateHttpSession(true)
+                                                .deleteCookies("JSESSIONID")
+                                                .permitAll())
+                                .csrf(AbstractHttpConfigurer::disable);
+                return http.build();
+        }
 }
--- src/main/java/com/phatdo/passwordmanager/Config/WebConfig.java
+++ src/main/java/com/phatdo/passwordmanager/Config/WebConfig.java
@@ -16,6 +16,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("/auth/login");
-        registry.addViewController("/logout");
+        registry.addViewController("/").setViewName("index");
     }
 }
