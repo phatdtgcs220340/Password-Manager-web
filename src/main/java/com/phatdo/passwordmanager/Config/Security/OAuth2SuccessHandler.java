8d142209b06eb3fb318ca3e3ca26cac29af87b8f
phatdtgcs220340@fpt.edu.vn <phatdtgcs220340@fpt.edu.vn>
2024-04-01 12:15:52 +0700

    add successful redirection

--- src/main/java/com/phatdo/passwordmanager/Config/Security/OAuth2SuccessHandler.java
+++ src/main/java/com/phatdo/passwordmanager/Config/Security/OAuth2SuccessHandler.java
@@ -2,21 +2,19 @@ package com.phatdo.passwordmanager.Config.Security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import com.phatdo.passwordmanager.Config.Security.CustomOAuth2.CustomOAuth2User;
-import com.phatdo.passwordmanager.Entity.User.User;
-import com.phatdo.passwordmanager.Entity.User.UserRepository;
 import com.phatdo.passwordmanager.Entity.User.UserService;
 
-import java.util.Map;
 import java.io.IOException;
-import java.util.List;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -27,14 +25,47 @@ import lombok.RequiredArgsConstructor;
 public class OAuth2SuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
         @Autowired
         private UserService userService;
+        private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
 
         @Override
         public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                         Authentication authentication) throws ServletException, IOException {
-                System.out.println("Chay duoc successHandler roi ne");
                 CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
                 super.onAuthenticationSuccess(request, response, authentication);
+                // save user after authentication
                 userService.processOAuthPostLogin(oAuth2User.getEmail(), oAuth2User.getName());
-                System.out.println("hehe xong successhandler roi");
+
+        }
+
+        protected void handle(
+                        HttpServletRequest request,
+                        HttpServletResponse response,
+                        Authentication authentication) throws IOException {
+
+                String targetUrl = determineTargetUrl(authentication);
+
+                if (response.isCommitted()) {
+                        logger.debug(
+                                        "Response has already been committed. Unable to redirect to "
+                                                        + targetUrl);
+                        return;
+                }
+
+                redirectStrategy.sendRedirect(request, response, targetUrl);
+        }
+
+        protected String determineTargetUrl(final Authentication authentication) {
+                Map<String, String> roleTargetUrlMap = new HashMap<>();
+                roleTargetUrlMap.put("ROLE_USER", "/home");
+
+                final Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+                for (final GrantedAuthority grantedAuthority : authorities) {
+                        String authorityName = grantedAuthority.getAuthority();
+                        if (roleTargetUrlMap.containsKey(authorityName)) {
+                                return roleTargetUrlMap.get(authorityName);
+                        }
+                }
+
+                throw new IllegalStateException();
         }
 }
