aec5b0f2e5aefc614d78fc60129828950994e43f
phatdtgcs220340@fpt.edu.vn <phatdtgcs220340@fpt.edu.vn>
2024-04-01 12:14:36 +0700

    add default constructor

--- src/main/java/com/phatdo/passwordmanager/Entity/User/User.java
+++ src/main/java/com/phatdo/passwordmanager/Entity/User/User.java
@@ -6,11 +6,16 @@ import java.util.stream.Collectors;
 import com.phatdo.passwordmanager.Entity.Application.Application;
 import com.phatdo.passwordmanager.Entity.Account.Account;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Entity
 @Data
 @Table(name = "\"user\"")
+@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PUBLIC, force = true)
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
